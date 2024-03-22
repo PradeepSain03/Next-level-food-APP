67f27144b5cf4af1e840d4814202dd5c19358ea6
Pradeep_sain_03 <130055570+PradeepSain03@users.noreply.github.com>
2024-03-22 10:38:52 +0530

    Add files via upload

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main style={{textAlign:"center",color:"white"}}>
+  
+      <p >Failed to fetch meal data. Please try again later.</p>
+      <h1>An error occurred!</h1>
+    </main>
+  );
+}
